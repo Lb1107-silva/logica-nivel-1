f7201083e3f3d67902110df3cf824885473965cf
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-08 21:39:22 -0300

    Create ex-16.py

---
+++ ex-16.py
@@ -0,0 +1,9 @@
+print("exercício 16")
+print("Número de maçãs compradas")
+valor1 = int(input())
+if valor1 >= 12:
+    print("valor das maçãs compradas R$:")
+    print(valor1*1)
+if valor1 < 12:
+        print("valor das maçãs compradas:")
+        print(valor1*1.30)
