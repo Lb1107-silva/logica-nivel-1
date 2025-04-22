0cbd1a0933a783284d0e8e9c14ce6e4b9b3e95ef
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-22 18:13:54 -0300

    Create ex-22.py

---
+++ ex-22.py
@@ -0,0 +1,8 @@
+print("exercicio 24)
+senha = 147
+print("qual a senha?")
+tentativa1 = float(input())
+if tentativa1 == senha:
+    print("senha está correta.")
+else: 
+    print("senha incorreta, tente novamente.")
