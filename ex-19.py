274b676fcc4787e0aede940aa2ba75e78f1bd453
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-22 13:26:09 -0300

    Create ex-19.py

---
+++ ex-19.py
@@ -0,0 +1,14 @@
+print("exercício 21")
+print("digite um valor:")
+valor1 = float(input())
+print("digite o segundo valor:")
+valor2 = float(input())
+print("digite o terceiro valor:")
+valor3 = float(input())
+print("digite o quarto valor:")
+valor4 = float(input())
+valores = [ valor1, valor2, valor3, valor4]
+valores.sort(reverse=True)
+soma = valores[0] , valores[1] , valores[2],  valores[3]
+print("ordem crescente:")
+print(soma)
