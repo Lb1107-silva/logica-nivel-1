b991c3160be713075802c7f7d0c73518068897f1
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-22 10:13:26 -0300

    Create ex-16.py

---
+++ ex-16.py
@@ -0,0 +1,9 @@
+print("exercício 18")
+print("qual sua nota?")
+valor1 = int(input())
+if valor1 >= 7:
+    print("aprovado")
+elif 5 <= valor1 <=6.9:
+    print("recuperação")
+else:
+    print("reprovado")
