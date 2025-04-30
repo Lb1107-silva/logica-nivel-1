decfc6e85f5b833d4e917d64f76fa34f53c7a7e7
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-29 21:23:24 -0300

    Create ex-26.py

---
+++ ex-26.py
@@ -0,0 +1,9 @@
+print("exercicio 28")
+valor1 = float(input("digite um numero:"))
+valor2 = float(input("digite um numero:"))
+valor3 = float(input("digite um numero:"))
+valor4 = float(input("digite um numero:"))
+valor5 = float(input("digite um numero:"))
+valores = [valor1 , valor2 , valor3 , valor4 , valor5]
+print("o primeiro digito foi:")
+print(valor2)
