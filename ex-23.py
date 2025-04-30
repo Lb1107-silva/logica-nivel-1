00f2c54bd04bff7f2ca42970f9d54471665e0888
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-29 21:15:19 -0300

    Create ex-23.py

---
+++ ex-23.py
@@ -0,0 +1,7 @@
+print("exercicio 25")
+numero = float(input("digite um numero:"))
+total = 0
+while numero > 0:
+    total = total + numero
+    numero = float(input("digite um numero"))
+print(total)
