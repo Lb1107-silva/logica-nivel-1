7e4215b624841e2a393d78a685f80ee28ab4c4ff
Lb1107-silva <lucas_11barbosa@hotmail.com>
2025-04-29 21:33:47 -0300

    Create ex-28.py

---
+++ ex-28.py
@@ -0,0 +1,7 @@
+print("exercicio 30")
+numeros = [1,2,3,4,5,5,5,5,5]
+contador = 5
+for numero in numeros:
+    if numero == 6:
+        contador = contador
+print("o numero 6 se repitiu ", contador , "vezes")
